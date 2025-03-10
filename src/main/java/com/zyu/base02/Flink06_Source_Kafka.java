35c0de08750ed9eab9743a3a2be4c9964f1c5cbf
zyu <zack98zy@163.com>
2025-03-10 17:50:28 +0800

    chore:补充代码

--- src/main/java/com/zyu/base02/Flink06_Source_Kafka.java
+++ src/main/java/com/zyu/base02/Flink06_Source_Kafka.java
@@ -1,8 +1,12 @@
 package com.zyu.base02;
 
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.api.common.serialization.SimpleStringSchema;
 import org.apache.flink.connector.kafka.source.KafkaSource;
 import org.apache.flink.connector.kafka.source.enumerator.initializer.OffsetsInitializer;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.kafka.clients.consumer.OffsetResetStrategy;
 
 /**
  * @author zyu
@@ -20,8 +24,21 @@ public class Flink06_Source_Kafka {
                 // 从最早位点开始消费
                 // .setStartingOffsets(OffsetsInitializer.earliest())
                 // 从最末尾位点开始消费
+                // .setStartingOffsets(OffsetsInitializer.latest())
+                // 从时间戳大于等于指定时间戳（毫秒）的数据开始消费
+                // .setStartingOffsets(OffsetsInitializer.timestamp(1657256176000L))
+                // 从消费组提交的位点开始消费，不指定位点重置策略
+                // .setStartingOffsets(OffsetsInitializer.committedOffsets())
+                // 从消费组提交的位点开始消费，如果提交位点不存在，使用最早位点
+                // .setStartingOffsets(OffsetsInitializer.committedOffsets(OffsetResetStrategy.EARLIEST))
                 .setStartingOffsets(OffsetsInitializer.latest())
-
+                .setValueOnlyDeserializer(new SimpleStringSchema())
                 .build();
+        // 封装为流
+        DataStreamSource<String> ds = env.fromSource(kafkaSource, WatermarkStrategy.noWatermarks(), "kafka_source");
+        // 打印输出
+        ds.print();
+        // 提交作业
+        env.execute();
     }
 }
