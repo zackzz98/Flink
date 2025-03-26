2e7122caf9a305ef20f9600487d858d84c238251
zyu <zack98zy@163.com>
2025-03-26 18:21:30 +0800

    first commit

--- src/main/java/com/zyu/state/Flink02_KeyedState_ValueState_2.java
+++ src/main/java/com/zyu/state/Flink02_KeyedState_ValueState_2.java
@@ -1,5 +1,7 @@
 package com.zyu.state;
 
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
 /**
  * @author zyu
  * date2025/3/25 18:37
@@ -7,6 +9,10 @@ package com.zyu.state;
  */
 public class Flink02_KeyedState_ValueState_2 {
     public static void main(String[] args) throws Exception {
+        // 创建执行环境
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(1);
 
+        //
     }
 }
